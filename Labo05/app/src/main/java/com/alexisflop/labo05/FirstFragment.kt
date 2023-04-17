a0c6250a23f16c0e8cb8ae146fd804996bcaa476
AlexisFlop <00079520@uca.edu.sv>
2023-04-17 13:02:30 -0600

    Adding labo

---
+++ Labo05/app/src/main/java/com/alexisflop/labo05/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.alexisflop.labo05
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var addMovieButton = view?.findViewById(R.id.add_movie_action) as Button
+
+        addMovieButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_firstFragment_to_thirdFragment)
+        }
+
+    }
+}
